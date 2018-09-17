bf5f7f29150708ee6516200eb954a072a45430ab
Greg Arakelian <greg@arakelian.com>
2018-09-17 14:24:49 -0400

    Fix unit test

--- src/test/java/com/arakelian/elastic/model/aggs/bucket/GeoHashGridAggregationTest.java
+++ src/test/java/com/arakelian/elastic/model/aggs/bucket/GeoHashGridAggregationTest.java
@@ -27,6 +27,7 @@ import com.arakelian.jackson.utils.JacksonTestUtils;
 public class GeoHashGridAggregationTest {
     public static final GeoHashGridAggregation MINIMAL = ImmutableGeoHashGridAggregation.builder() //
             .name("my_aggregation") //
+            .field("field") //
             .build();
 
     @Test
